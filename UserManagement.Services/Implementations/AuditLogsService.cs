3c1e8eebeead81e1943dae2fd89daac0a7d492d6
Som Shrestha <som_shrestha@hotmail.co.uk>
2025-08-11 19:04:34 +0100

    Remove of unused usings

--- UserManagement.Services/Implementations/AuditLogsService.cs
+++ UserManagement.Services/Implementations/AuditLogsService.cs
@@ -1,7 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using UserManagement.Data;
 using UserManagement.Data.Entities;
 using UserManagement.Services.Domain.Interfaces;
